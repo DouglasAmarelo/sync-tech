2f288a57566d8dde075e669f79ec166c0be87e39
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2023-02-15 15:50:31 -0300

    change the logic to luck

--- index.js
+++ index.js
@@ -1,50 +1,19 @@
-const workSchedule = [
-  'Joao Iglesias',
-  'Matheus Cruz',
-  'Rodrigo Leme',
-  'Tiago Kamegasawa',
-  'Yuri Martins',
+const people = [
+  'Amarelo',
+  'João',
+  'Theus',
+  'Painho',
+  'Ronin',
+  'Tiaguinho',
+  'Yuri',
 ];
 
+const getRandomPerson = (peopleList = people) =>
+  peopleList[Math.floor(Math.random() * peopleList.length)];
+
 const comigoNaoMorreu = () => {
-  const filteredSynchronizer = workSchedule.filter(
-    name => name != getTheSynchronizer()
-  );
-  const newSynchronizer =
-    filteredSynchronizer[
-      Math.floor(Math.random() * filteredSynchronizer.length)
-    ];
-
+  const newSynchronizer = getRandomPerson();
   updateTankerName(newSynchronizer);
-};
-
-const getDateFromUrl = () => {
-  const urlParams = new URLSearchParams(window?.location?.search);
-  const dateFromParams = urlParams.get('date');
-  const isAValidDate = String(dateFromParams).match(/\d{4}-\d{2}-\d{2}/gim);
-
-  return isAValidDate ? dateFromParams : null;
-};
-
-const generateDate = () => {
-  const customDate = getDateFromUrl();
-  const date = customDate ? new Date(customDate) : new Date();
-  const today = date.getUTCDate();
-  const dayOfTheWeek = date.getUTCDay() - 1;
-  const weekOfTheMonth = Math.ceil((today - dayOfTheWeek) / 7) - 1;
-
-  return {
-    today,
-    dayOfTheWeek,
-    weekOfTheMonth,
-  };
-};
-
-const getTheSynchronizer = () => {
-  const { dayOfTheWeek } = generateDate();
-  const theSynchronizer = workSchedule[dayOfTheWeek];
-
-  return theSynchronizer;
 };
 
 const updateTankerName = (name = 'Você!') => {
@@ -53,7 +22,7 @@ const updateTankerName = (name = 'Você!') => {
 };
 
 const start = () => {
-  const theSynchronizer = getTheSynchronizer();
+  const theSynchronizer = getRandomPerson();
 
   updateTankerName(theSynchronizer);
 };
